f463b7a8d3cb32622c1b337e02783ecef2dca0d0
Sylvain K <skocher@avb202.com>
2023-05-11 20:36:43 +0200

    Initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void should_test_work() {
+        assertThat(true).isTrue();
+    }
+
+}
